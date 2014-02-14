a7535855fbe752d540ab759a2e09539b377f0c0a
Jake Weinstein <xboxlover360@gmail.com>
2014-02-14 11:40:45 -0600

    Move RIL defs to common

--- device.mk
+++ device.mk
@@ -27,17 +27,6 @@ DEVICE_PACKAGE_OVERLAYS += device/lge/gee-common/overlay
 # Inherit from gee-common
 $(call inherit-product, device/lge/gee-common/gee-common.mk)
 
-# CDMA permissions
-PRODUCT_COPY_FILES += \
-	frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml
-
 #NFC firmware
 PRODUCT_COPY_FILES += \
     device/lge/gee/libpn544_fw.so:system/vendor/firmware/libpn544_fw.so
-
-# Telephony Properties
-PRODUCT_PROPERTY_OVERRIDES += \
-    telephony.lteOnCdmaDevice=1 \
-    telephony.lte.cdma.device=1 \
-    ro.telephony.default_network=10 \
-    ro.ril.def.preferred.network=10
