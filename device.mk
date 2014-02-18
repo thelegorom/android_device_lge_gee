4d16afe0b15281dbf7d0dad9827e1e5eb0053d19
Jake Weinstein <xboxlover360@gmail.com>
2014-02-18 14:52:44 -0600

    gee: No need to copy NFC Firmware, kernel firmware works on all variants
    Thank you very much to David Hacker.

--- device.mk
+++ device.mk
@@ -26,7 +26,3 @@ DEVICE_PACKAGE_OVERLAYS += device/lge/gee-common/overlay
 
 # Inherit from gee-common
 $(call inherit-product, device/lge/gee-common/gee-common.mk)
-
-#NFC firmware
-PRODUCT_COPY_FILES += \
-    device/lge/gee/libpn544_fw.so:system/vendor/firmware/libpn544_fw.so
