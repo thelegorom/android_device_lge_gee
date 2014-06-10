dcda18d724647ca3e85502bf8952a0695a703c82
cameron581 <cameron581@cox.net>
2014-06-10 07:54:02 -0500

    forgot about this

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,7 +13,6 @@
 # limitations under the License.
 
 TARGET_KERNEL_CONFIG := gee_defconfig
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := sm-arm-eabi-4.9
 
 # inherit from gee-common
 -include device/lge/gee-common/BoardConfigCommon.mk
